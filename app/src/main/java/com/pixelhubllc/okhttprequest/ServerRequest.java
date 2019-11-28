b85c8124b47a5df97430ad451819bfefa628e6aa
Mahmudul Hasan Tarek <tarekint1@gmail.com>
2019-11-28 15:35:55 +0600

    commit

--- app/src/main/java/com/pixelhubllc/okhttprequest/ServerRequest.java
+++ app/src/main/java/com/pixelhubllc/okhttprequest/ServerRequest.java
@@ -7,11 +7,13 @@ import android.view.textclassifier.TextLinks;
 import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -23,6 +25,7 @@ import okhttp3.Response;
 public class ServerRequest {
     private String ServerResponse;
     HashMap<String,String>mymap;
+    HashMap<String,HashMap<String,String>> mymap1;
     public boolean isCompleted=false;
 
     public ServerRequest(String URL, Context context){
@@ -41,30 +44,65 @@ public class ServerRequest {
                 Log.e("server response",ServerResponse);
                 mymap=new HashMap<>();
 
-                HashMap<String,HashMap<String,String>> mymap2;
 
+//                try {
+//                    JSONObject jsonObject=new JSONObject(ServerResponse);
+//                        Log.e("jsonobject",jsonObject.toString());
+//                        JSONObject jsonObject1=jsonObject.getJSONObject("data");
+//
+//                        mymap.put("id",jsonObject1.getString("id"));
+//                        mymap.put("email",jsonObject1.getString("email"));
+//                        mymap.put("first_name",jsonObject1.getString("first_name"));
+//                        mymap.put("last_name",jsonObject1.getString("last_name"));
+//                        mymap.put("avatar",jsonObject1.getString("avatar"));
+//
+//
+//
+//                        Log.e("id",mymap.get("id"));
+//                        Log.e("email",mymap.get("email"));
+//                        Log.e("firstname",mymap.get("first_name"));
+//                    Log.e("lastname",mymap.get("last_name"));
+//                    Log.e("avatar",mymap.get("avatar"));
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
 
-
+                JSONObject jsonObject;
+                JSONArray jsonArray;
+                mymap1=new HashMap<>();
                 try {
-                    JSONObject jsonObject=new JSONObject(ServerResponse);
-                        Log.e("jsonobject",jsonObject.toString());
-                        JSONObject jsonObject1=jsonObject.getJSONObject("data");
 
-                        mymap.put("id",jsonObject1.getString("id"));
-                        mymap.put("email",jsonObject1.getString("email"));
-                        mymap.put("first_name",jsonObject1.getString("first_name"));
-                        mymap.put("last_name",jsonObject1.getString("last_name"));
-                        mymap.put("avatar",jsonObject1.getString("avatar"));
+                    jsonObject = new JSONObject(ServerResponse);
+                    Log.e("json object", jsonObject.toString());
+                    mymap.put("page",jsonObject.getString("page"));
+                    mymap.put("per_page",jsonObject.getString("per_page"));
+                    mymap.put("total",jsonObject.getString("total"));
+                    mymap.put("total_pages",jsonObject.getString("total_pages"));
+                    mymap.put("data",jsonObject.getString("data"));
 
+                    Log.e("page",mymap.get("page"));
+                    Log.e("per_page",mymap.get("per_page"));
+                    Log.e("total",mymap.get("total"));
+                    Log.e("total_pages",mymap.get("total_pages"));
+                    Log.e("data",mymap.get("data"));
 
+                    jsonArray=jsonObject.getJSONArray("data");
 
-                        Log.e("id",mymap.get("id"));
-                        Log.e("email",mymap.get("email"));
-                        Log.e("firstname",mymap.get("first_name"));
-                    Log.e("lastname",mymap.get("last_name"));
-                    Log.e("avatar",mymap.get("avatar"));
-
-                } catch (JSONException e) {
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject jsonObject1=jsonArray.getJSONObject(i);
+                        HashMap<String,String> map=new HashMap<>();
+                        map.put("id",jsonObject1.getString("id"));
+                        map.put("name",jsonObject1.getString("name"));
+                        map.put("year",jsonObject1.getString("year"));
+                        map.put("color",jsonObject1.getString("color"));
+                        map.put("pantone_value",jsonObject1.getString("pantone_value"));
+                        mymap1.put(String.valueOf(i),map);
+                    }
+                    for(int j=0;j<mymap1.size();j++){
+                        Log.e("data",mymap1.get(String.valueOf(j)).toString());
+                    }
+                } catch (JSONException e){
                     e.printStackTrace();
                 }
 
@@ -73,9 +111,9 @@ public class ServerRequest {
                 ((Activity)context).runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(context,mymap.get("email"),Toast.LENGTH_SHORT).show();
-                        MainActivity.serverresponse=mymap.get("email");
-                        MainActivity.fun();
+                        //Toast.makeText(context,mymap.get("email"),Toast.LENGTH_SHORT).show();
+                        //MainActivity.serverresponse=mymap.get("email");
+                        //MainActivity.fun();
                     }
                 });
 
